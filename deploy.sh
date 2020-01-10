d51f6e5da68b8e52fbe81a0400cb66fb67f3403e
Martin Jaeger <17674105+martinjaeger@users.noreply.github.com>
2020-01-10 14:04:29 +0100

    New try with gh-pages moved to master

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 #
 # Copyright (c) 2019 Martin Jäger
 #
-# This script deploys master branch commits to gh-pages root directory and other branches to
+# This script deploys source branch commits to master root directory and other branches to
 # sub-directory /branches/<branch-name>.
 #
 # Rendered branches can be accessed via https://dc-grid.github.io/branch/<branch-name>
@@ -18,39 +18,39 @@ if [ $TRAVIS_PULL_REQUEST != "false" ]; then
     printf "\nPreparing deployment for pull-request #$TRAVIS_PULL_REQUEST\n"
     sed -i -e "s/base: '\/'/base: '\/pr\/$TRAVIS_PULL_REQUEST\/'/g" docs/.vuepress/config.js
     sed -i -e "s^docsRepo: 'dc-grid/dc-grid.github.io'^docsRepo: '$TRAVIS_PULL_REQUEST_SLUG'^g" docs/.vuepress/config.js
-    sed -i -e "s/docsBranch: 'master'/docsBranch: '$TRAVIS_BRANCH'/g" docs/.vuepress/config.js
-elif [ $TRAVIS_BRANCH != "master" ]; then
+    sed -i -e "s/docsBranch: 'source'/docsBranch: '$TRAVIS_BRANCH'/g" docs/.vuepress/config.js
+elif [ $TRAVIS_BRANCH != "source" ]; then
     printf "\nPreparing deployment for branch $TRAVIS_BRANCH\n"
     sed -i -e "s/base: '\/'/base: '\/branch\/$TRAVIS_BRANCH\/'/g" docs/.vuepress/config.js
-    sed -i -e "s/docsBranch: 'master'/docsBranch: '$TRAVIS_BRANCH'/g" docs/.vuepress/config.js
+    sed -i -e "s/docsBranch: 'source'/docsBranch: '$TRAVIS_BRANCH'/g" docs/.vuepress/config.js
 else
-    printf "\nPreparing deployment for master\n"
+    printf "\nPreparing deployment for source\n"
 fi
 
 # build the website
 npm run docs:build
 printf "\n\n"
 
-# get previous gh-pages deployment including all PR and branch folders
-rm -rf gh-pages
-git clone -b gh-pages https://github.com/dc-grid/dc-grid.github.io gh-pages
+# get previous master deployment including all PR and branch folders
+rm -rf master
+git clone -b master https://github.com/dc-grid/dc-grid.github.io master
 
 # compile folders correctly
 if [ $TRAVIS_PULL_REQUEST != "false" ]; then
-    mkdir -p gh-pages/pr
-    rm -rf "gh-pages/pr/$TRAVIS_PULL_REQUEST"
-    cp -r docs/.vuepress/dist "gh-pages/pr/$TRAVIS_PULL_REQUEST"
-elif [ $TRAVIS_BRANCH != "master" ]; then
-    mkdir -p gh-pages/branch
-    rm -rf "gh-pages/branch/$TRAVIS_BRANCH"
-    cp -r docs/.vuepress/dist "gh-pages/branch/$TRAVIS_BRANCH"
+    mkdir -p master/pr
+    rm -rf "master/pr/$TRAVIS_PULL_REQUEST"
+    cp -r docs/.vuepress/dist "master/pr/$TRAVIS_PULL_REQUEST"
+elif [ $TRAVIS_BRANCH != "source" ]; then
+    mkdir -p master/branch
+    rm -rf "master/branch/$TRAVIS_BRANCH"
+    cp -r docs/.vuepress/dist "master/branch/$TRAVIS_BRANCH"
 else
-    if [ -d "gh-pages/branch" ]; then
-        mv gh-pages/pr docs/.vuepress/dist/
-        mv gh-pages/branch docs/.vuepress/dist/
+    if [ -d "master/branch" ]; then
+        mv master/pr docs/.vuepress/dist/
+        mv master/branch docs/.vuepress/dist/
     fi
-    rm -rf gh-pages
-    mv docs/.vuepress/dist gh-pages
+    rm -rf master
+    mv docs/.vuepress/dist master
 fi
 
 echo "Preparation and build done."
